fd40c3bb9c135db5bcf83ce5bc8e17cec5caa0ff
Suneel Freimuth <suneelfreimuth1@gmail.com>
2023-04-15 13:09:39 -0700

    Add LogRequests and variants

--- pkg/middleware/middleware.go
+++ pkg/middleware/middleware.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -10,7 +11,7 @@ import (
 // Returns an *http.Request; the middleware can set router params or reject a request by returning nil.
 type Middleware func(http.ResponseWriter, *http.Request) *http.Request
 
-// ExpectQueryParam returns a middleware that checks for the presence of a query parameter.
+// Returns a middleware that checks for the presence of a query parameter.
 func ExpectQueryParam(name string) Middleware {
 	return func(w http.ResponseWriter, r *http.Request) *http.Request {
 		if r.URL.Query().Has(name) {
@@ -20,4 +21,27 @@ func ExpectQueryParam(name string) Middleware {
 		fmt.Fprintf(w, "Missing query param: %s", name)
 		return nil
 	}
+}
+
+// Returns a middleware that logs the details of an incoming request.
+func LogRequests(w io.Writer) Middleware {
+	return func(_ http.ResponseWriter, r *http.Request) *http.Request {
+		logRequest(w, r)
+		return r
+	}
+}
+
+// Returns a middleware that logs the details of an incoming request only if
+// test(request) == true.
+func LogRequestsIf(test func(*http.Request) bool, w io.Writer) Middleware {
+	return func(_ http.ResponseWriter, r *http.Request) *http.Request {
+		if test(r) {
+			logRequest(w, r)
+		}
+		return r
+	}
+}
+
+func logRequest(w io.Writer, r *http.Request) {
+	fmt.Fprintf(w, "%s %v\n", r.Method, r.URL)
 }
--- pkg/middleware/middleware_test.go
+++ pkg/middleware/middleware_test.go
@@ -3,13 +3,14 @@ package middleware
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
 func TestExpectQueryParam(t *testing.T) {
 	t.Run("foo=bar", func(t *testing.T) {
 		m := ExpectQueryParam("foo")
-		r, _ := http.NewRequest("GET", "http://example.com?foo=bar", nil)
+		r := httptest.NewRequest("GET", "http://example.com?foo=bar", nil)
 		w := httptest.NewRecorder()
 		if m(w, r) != r {
 			t.Error("ExpectQueryParam did not recognize foo was provided")
@@ -18,7 +19,7 @@ func TestExpectQueryParam(t *testing.T) {
 
 	t.Run("foo=", func(t *testing.T) {
 		m := ExpectQueryParam("foo")
-		r, _ := http.NewRequest("GET", "http://example.com?foo=", nil)
+		r := httptest.NewRequest("GET", "http://example.com?foo=", nil)
 		w := httptest.NewRecorder()
 		if m(w, r) != r {
 			t.Error("ExpectQueryParam did not recognize foo was provided")
@@ -27,7 +28,7 @@ func TestExpectQueryParam(t *testing.T) {
 
 	t.Run("foo without equals sign", func(t *testing.T) {
 		m := ExpectQueryParam("foo")
-		r, _ := http.NewRequest("GET", "http://example.com?foo", nil)
+		r := httptest.NewRequest("GET", "http://example.com?foo", nil)
 		w := httptest.NewRecorder()
 		if m(w, r) != r {
 			t.Error("ExpectQueryParam should not have recognized foo was provided")
@@ -36,10 +37,70 @@ func TestExpectQueryParam(t *testing.T) {
 
 	t.Run("foo absent", func(t *testing.T) {
 		m := ExpectQueryParam("foo")
-		r, _ := http.NewRequest("GET", "http://example.com?bar=foo", nil)
+		r := httptest.NewRequest("GET", "http://example.com?bar=foo", nil)
 		w := httptest.NewRecorder()
 		if m(w, r) != nil {
 			t.Error("ExpectQueryParam should not have recognized foo was provided")
+		}
+	})
+}
+
+func TestLogRequests(t *testing.T) {
+	t.Run("log requests", func(t *testing.T) {
+		var builder strings.Builder
+		mw := LogRequests(&builder)
+		r := mw(nil, httptest.NewRequest(http.MethodGet, "https://example.com/", nil))
+		if r == nil {
+			t.Fatal("request was nil, should be defined")
+		}
+
+		result := builder.String()
+		if result != "GET https://example.com/\n" {
+			t.Fatalf(
+				"incorrect log for request %s %s:\n  got %v",
+				r.Method,
+				r.URL.String(),
+				result,
+			)
+		}
+	})
+
+	t.Run("log requests if", func(t *testing.T) {
+		var builder strings.Builder
+		methodIsGet := func(r *http.Request) bool {
+			return r.Method == http.MethodGet
+		}
+		mw := LogRequestsIf(methodIsGet, &builder)
+
+		r := mw(nil, httptest.NewRequest(http.MethodGet, "https://example.com/", nil))
+		if r == nil {
+			t.Fatal("request was nil, should be defined")
+		}
+
+		result := builder.String()
+		if result != "GET https://example.com/\n" {
+			t.Fatalf(
+				"incorrect log for request %s %s:\n  got %v",
+				r.Method,
+				r.URL.String(),
+				result,
+			)
+		}
+
+		builder.Reset()
+		r = mw(nil, httptest.NewRequest(http.MethodPost, "https://example.com/", nil))
+		if r == nil {
+			t.Fatal("request was nil, should be defined")
+		}
+
+		result = builder.String()
+		if result != "" {
+			t.Fatalf(
+				"incorrect log for request %s %s:\n  got %v",
+				r.Method,
+				r.URL.String(),
+				result,
+			)
 		}
 	})
 }
