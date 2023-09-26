9ab94d18d96912a0f674713c87a3feec6aab028b
jakenichols2719 <mail@jnichols.info>
2023-09-25 18:24:47 -0600

    add request ID accessors
    Signed-off-by: jakenichols2719 <mail@jnichols.info>

--- pkg/middleware/requestid.go
+++ pkg/middleware/requestid.go
@@ -35,3 +35,20 @@ func RequestID(generator RequestIDGenerator, key string, isContext bool) Middlew
 		return r
 	}
 }
+
+// GetRequestIDHeader gets the request ID from a request if it was set in the header.
+// Headers don't require any special behavior to access; this function mostly exists for consistency with
+// GetRequestIDContext.
+func GetRequestIDHeader(req *http.Request, key string) string {
+	return req.Header.Get(key)
+}
+
+// GetRequestIDContext gets the request ID from a request if it was set in context.
+// Since the key is typed, you *must* use this function to access the request ID. It's recommended that you
+// wrap this with whatever key you use for your project as a convenience.
+func GetRequestIDContext(req *http.Request, key string) string {
+	if rid, ok := req.Context().Value(requestIdKey(key)).(string); ok {
+		return rid
+	}
+	return ""
+}
